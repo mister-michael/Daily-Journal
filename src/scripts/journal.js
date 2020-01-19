62769aee82a207557b856d49aed193e4821ab951
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-18 22:46:42 -0600

    refined comments for clarity and aesthetics

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,11 +2,13 @@ let journalEntryOutput = [];
 
 const journalEntry = {
 
-    entry: ["abc123", 255, "hello"] //test key
+    //entry: ["abc123", 255, "hello"] 
+    //test
 }
 
 journalEntryOutput.push(journalEntry);
 
-console.log(journalEntryOutput);  //test log
+//console.log(journalEntryOutput);  
+//test
 
 //it works
