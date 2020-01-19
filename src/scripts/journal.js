7935d0fc0c701bcde243119ed6cb89a074a8d7b1
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-18 22:39:14 -0600

    build objects and function to push journalEntry into journalEntryOutput, comments indicate data entered for console test

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,1 +1,12 @@
-let hello ="World"
+let journalEntryOutput = [];
+
+const journalEntry = {
+
+    entry: ["abc123", 255, "hello"] //test key
+}
+
+journalEntryOutput.push(journalEntry);
+
+console.log(journalEntryOutput);  //test log
+
+//it works
