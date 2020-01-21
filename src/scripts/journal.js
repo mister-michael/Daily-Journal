3b53a3b2f7f8db51f88ed4efe775188f198982f8
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-21 15:14:32 -0600

    created function to create DOM elements filled with journal entry properties

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,14 +1,32 @@
-let journalEntryOutput = [];
-
-const journalEntry = {
-
-    entry: ["abc123", 255, "hello"] 
-    //test
+const journalEntry =
+{
+    date: "01/21/2020",
+    concept: "JSON",
+    content: "We learned about JSON files",
+    mood: "sublime",
 }
 
-journalEntryOutput.push(journalEntry);
+let journalEntries = [];
 
-console.log(journalEntryOutput);  
-//test
+journalEntries.push(journalEntry);
 
-//it works
+const makeJournalEntryComponent = (journalEntry) => {
+    return `
+   <h2>${journalEntry.concept}</h2>
+   <div>${journalEntry.date}</div>
+   <article>${journalEntry.content}</article>
+   <div>${journalEntry.mood}</div>
+   `
+};
+
+const renderJournalEntries = (entries) => {
+    const targetLocation = document.querySelector(".entryLog");
+    for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i];
+        targetLocation.innerHTML += makeJournalEntryComponent(entry);
+    }
+}
+
+renderJournalEntries(journalEntries);
+
+console.log(journalEntries)
