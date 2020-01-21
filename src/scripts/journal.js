d78875dde884138c477cdc391f25babea6431287
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-21 13:27:22 -0600

    deleteed some empty space

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,13 +2,13 @@ let journalEntryOutput = [];
 
 const journalEntry = {
 
-    //entry: ["abc123", 255, "hello"] 
+    entry: ["abc123", 255, "hello"] 
     //test
 }
 
 journalEntryOutput.push(journalEntry);
 
-//console.log(journalEntryOutput);  
+console.log(journalEntryOutput);  
 //test
 
 //it works
