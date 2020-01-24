ebcedf7f1b06850b8d51de861bc0666ae55ae6ee
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-24 11:47:30 -0600

    cleaned up comments and debris and formatted code

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,15 +1,3 @@
-// const journalEntry =
-// {
-//     date: "01/21/2020",
-//     concept: "JSON",
-//     content: "We learned about JSON files",
-//     mood: "sublime",
-// }
-
-// let journalEntries = [];
-
-// journalEntries.push(journalEntry);
-
 const makeHTML = (journalEntry) => {
     return `
    <h2>${journalEntry.concept}</h2>
@@ -19,21 +7,13 @@ const makeHTML = (journalEntry) => {
    `
 };
 
-// const renderJournalEntries = (entries) => {
-    const targetLocation = document.querySelector(".entryLog");
-//     for (let i = 0; i < entries.length; i++) {
-//         const entry = entries[i];
-//         targetLocation.innerHTML += makeJournalEntryComponent(entry);
-//     }
-// }
-
-// renderJournalEntries(journalEntries);
-
-// console.log(journalEntries)
+const targetLocation = document.querySelector(".entryLog");
 
 fetch("http://localhost:8088/entries")
     .then(responseFromApi => responseFromApi.json())
     .then(parsedDataFromApi => {
         console.table(parsedDataFromApi)
         parsedDataFromApi.forEach(entry => {
-            targetLocation.innerHTML += makeHTML(entry)})})
+            targetLocation.innerHTML += makeHTML(entry)
+        })
+    })
