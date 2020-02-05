640a0cc4a251a3164706824cff273336e140b05b
Michael Clark <michaelclarknashville@protonmail.com>
2020-02-05 13:56:46 -0600

    added a listener event to filter entries.jsson and print to dom

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -5,3 +5,43 @@ import addEventListener from "./addEventListener.js"
 addEventListener();
 
 API.getJournalEntries().then(renderHTML)
+
+const targetJuicedFilter = document.getElementById("juiced-filter")
+const targetStokedFilter = document.getElementById("stoked-filter")
+const targetOkFilter = document.getElementById("ok-filter")
+const targetBummedFilter = document.getElementById("bummed-filter")
+const targetNhilisticFilter = document.getElementById("nhilistic-filter")
+
+
+// targetJuicedFilter.addEventListener("click", event => {
+//     const juiced = event.target.value;
+//     console.log(juiced)
+
+// })
+const filterButtonArray = document.getElementsByName("moodFieldset")
+console.log(filterButtonArray)
+
+filterButtonArray.forEach(button => {
+    button.addEventListener("click", event => {
+        const mood = event.target.value;
+        console.log(event)
+        API.getJournalEntries()
+            .then(entries => {
+                renderHTML(entries.filter(element => element.mood === mood))
+            })
+    })})
+
+// targetOkFilter.addEventListener("click", event => {
+//     const ok = event.target.value;
+//     console.log(ok)
+// })
+
+// targetBummedFilter.addEventListener("click", event => {
+//     const bummed = event.target.value;
+//     console.log(bummed)
+// })
+
+// targetNhilisticFilter.addEventListener("click", event => {
+//     const nhilistic = event.target.value;
+//     console.log(nhilistic)
+// })
